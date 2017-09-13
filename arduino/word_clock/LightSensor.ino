1f7f9244cc94951e45d6347e559307fe6f198ff1
Sasa Sekulic <ja@sasasekulic.info>
2017-09-13 23:08:59 +0200

    fixed light sensor setup

--- arduino/word_clock/LightSensor.ino
+++ arduino/word_clock/LightSensor.ino
@@ -1,9 +1,8 @@
-#define LIGHT_SENSOR_DATA_PIN A0
-const float LIGHT_SENSOR_MAX_VALUE = 1023.0; // The light sensor returns values in the range 0 - 1023. Check your sensor docs!
+const float LIGHT_SENSOR_MAX_VALUE = 1023.0; // The light sensor returns values in the range 0 - 1023, the higher the value the darker it is. Check your sensor docs!
 
 void loopLightSensor() {    
     float rawValue = analogRead(LIGHT_SENSOR_DATA_PIN);
-    ambientBrightness = rawValue / LIGHT_SENSOR_MAX_VALUE;
+    ambientBrightness = 1 - (rawValue / LIGHT_SENSOR_MAX_VALUE); //we need to normalize the value and then invert it; max brightness during day
     Serial.print("light sensor value =  "); Serial.print(rawValue); Serial.print(", normalized =  "); Serial.print(ambientBrightness);    
     Serial.println();
 }
--- arduino/word_clock/word_clock.ino
+++ arduino/word_clock/word_clock.ino
@@ -3,22 +3,23 @@
 #include <Adafruit_NeoPixel.h>    //https://github.com/adafruit/Adafruit_NeoPixel
 #include <RTClib.h>  
 
+#define LIGHT_SENSOR_DATA_PIN A0
 #define PIXELS_DATA_PIN D5
 const int LEDS_COUNT = 144;
 const int LEDS_SIDE = 12;
 const int LED_REFRESH_RATE_IN_MS = 2000;
 const int WIFI_SETUP_BUTTON_PIN = 0; 
 
-float ambientBrightness = 0.2;         //default is 20% brightness
+float ambientBrightness = 1.0;         //default is 100% brightness
 
 boolean wifiSetupRequested = false;    //TODO Wifi setup button
-boolean useLightSensor = false;        //TODO how to automatically check light sensor data and enable it if correct?
 boolean displayUpdated = false;
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);         // Initialize the output led
   pinMode(WIFI_SETUP_BUTTON_PIN, INPUT);// Initialize the WIFI setup button 
+  pinMode(LIGHT_SENSOR_DATA_PIN, INPUT);// Initialize the light sensor
 
   scanPorts();
 
@@ -46,10 +47,7 @@ void loop() {
     setupWiFi();
   }
   
-  if (useLightSensor) {
-    loopLightSensor();
-  }
-
+  loopLightSensor();
   loopNTP();
   loopDisplayer();
 
